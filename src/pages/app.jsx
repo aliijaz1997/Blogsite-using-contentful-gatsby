557bc0c93e8f74a6cb93d389129aa02963e2b47e
aliijaz1997 <57563582+aliijaz1997@users.noreply.github.com>
2020-12-05 02:23:04 +0500

    Update app.jsx

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -2,15 +2,9 @@ import React from "react"
 import { Router } from "@reach/router"
 import Layout from "../components/Layout"
 import Login from "../components/Login"
-// import PrivateRoute from "../components/PrivateRoute"
-import Status from "../components/Status"
-// import MainContainer from "../components/mainandcombinedcomponents"
-// import Blog from "./blog"
 const App  = () => (
   <Layout>
-    <Status />
     <Router>
-      {/* <PrivateRoute path="/app/profile" component={Blog} /> */}
       <Login path="/app/login" />
     </Router>
   </Layout>

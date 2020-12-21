615bc9d2cd2d56a26086350483037ada3a8da30f
Hifz ur Rehman Ali <aliijaz2010@yahoo.com>
2020-12-22 03:53:20 +0500

    Final commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    // `gatsby-plugin-firebase-app` ,
     {
       resolve: `gatsby-plugin-material-ui`,
       options: {
@@ -36,13 +37,13 @@ module.exports = {
       resolve: "gatsby-plugin-firebase",
       options: {
         credentials: {
-          apiKey: process.env.GATSBY_FIREBASE_API_KEY,
-          authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
-          databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
-          projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
-          storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
-          messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
-          appId: process.env.GATSBY_FIREBASE_APP_ID,
+          apiKey: "AIzaSyDM7VpbgykCjVab_3oimEzGu5BzmE03HOQ",
+          authDomain: "blog-site-a3d9d.firebaseapp.com",
+          databaseURL: "https://blog-site-a3d9d.firebaseio.com",
+          projectId: "blog-site-a3d9d",
+          storageBucket: "blog-site-a3d9d.appspot.com",
+          messagingSenderId: "486164789431",
+          appId: "1:486164789431:web:4943eb6b840c1c217ff532"
         }
       }
     },
